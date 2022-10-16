d6084aaa557dab6cbde4a875cc0b6e855202c7f0
Daniel Lee <daniel.lee@circleci.com>
2022-10-16 15:33:39 -0700

    search block by number or hash

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,22 @@
 import { GoSearch } from "react-icons/go";
-import { getBlock } from "../api/extract";
+import { getBlock, getLatestBlock } from "../api/extract";
+import { useState } from "react";
 
 const Search = () => {
+    const [blockNum, setBlockNum] = useState();
+
     async function printBlock() {
-        let block = await getBlock();
-        console.log(block);
+        if (!blockNum) {
+            let block = await getLatestBlock();
+            console.log(block);
+        } else {
+            let block = await getBlock(blockNum);
+            console.log(block);
+        }
+    }
+
+    function handleInput(e) {
+        setBlockNum(e.target.value);
     }
 
     return (
@@ -13,6 +25,7 @@ const Search = () => {
                 type="text"
                 placeholder="Search by block number / hash"
                 className="border-2 w-1/3 placeholder:text-sm"
+                onChange={handleInput}
             ></input>
             <div className="border-2 h-7 w-8 flex items-center justify-center">
                 <GoSearch onClick={printBlock}></GoSearch>
