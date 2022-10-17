fd43d7ea6ec74bf430a7a97c1aec4dfc5fa5332a
Daniel Lee <daniel.lee@circleci.com>
2022-10-16 19:58:32 -0700

    search with hash or block

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,22 @@
 import { GoSearch } from "react-icons/go";
 import { getBlock, getLatestBlock } from "../api/extract";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Block from "./Block";
 
 const Search = () => {
     const [blockNum, setBlockNum] = useState();
+    const [block, setBlock] = useState();
 
-    async function printBlock() {
+    async function queryBlock() {
+        let blockData;
+
         if (!blockNum) {
-            let block = await getLatestBlock();
-            console.log(block);
+            blockData = await getLatestBlock();
         } else {
-            let block = await getBlock(blockNum);
-            console.log(block);
+            blockData = await getBlock(blockNum);
         }
+
+        setBlock(blockData);
     }
 
     function handleInput(e) {
@@ -20,15 +24,20 @@ const Search = () => {
     }
 
     return (
-        <div className="flex justify-center items-center mt-10 w-100%">
-            <input
-                type="text"
-                placeholder="Search by block number / hash"
-                className="border-2 w-1/3 placeholder:text-sm"
-                onChange={handleInput}
-            ></input>
-            <div className="border-2 h-7 w-8 flex items-center justify-center">
-                <GoSearch onClick={printBlock}></GoSearch>
+        <div className="flex flex-col w-100% items-center gap-10">
+            <div className="flex justify-center items-center mt-10 w-2/3">
+                <input
+                    type="text"
+                    placeholder="Search by block number / hash"
+                    className="border-2 w-2/3 placeholder:text-sm"
+                    onChange={handleInput}
+                ></input>
+                <div className="border-2 h-7 w-8 flex items-center justify-center">
+                    <GoSearch onClick={queryBlock}></GoSearch>
+                </div>
+            </div>
+            <div className="w-2/3">
+                {block ? <Block block={block} /> : null}
             </div>
         </div>
     );
