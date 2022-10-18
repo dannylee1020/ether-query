b339d7b3e5f9c27161cfe989e62c8a5d0c0fdf51
Daniel Lee <daniel.lee@circleci.com>
2022-10-17 20:50:19 -0700

    add routing to different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Search from "./components/Search";
+import Transactions from "./components/Transactions";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 export default function App() {
     return (
-        <div>
-            <Search></Search>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Search />}></Route>
+                <Route path="/transactions" element={<Transactions />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
