9aeefa3d791ff47739315939d04cd0fd123ed6bf
Daniel Lee <daniel.lee@circleci.com>
2022-10-19 15:11:17 -0700

    add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Search from "./components/Search";
 import Transactions from "./components/Transactions";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App() {
+    const queryClient = new QueryClient();
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Search />}></Route>
-                <Route path="/transactions" element={<Transactions />}></Route>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Search />}></Route>
+                    <Route
+                        path="/transactions"
+                        element={<Transactions />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 }
