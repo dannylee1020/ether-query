a82f399c9b9b71edd600ad5221cb005f7be7b35f
Daniel Lee <daniel.lee@circleci.com>
2022-11-01 21:02:42 -0700

    configure theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    // darkMode: ["class", '[data-theme="dark"]'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    ...require("daisyui/src/colors/themes")[
+                        "[data-theme=wireframe]"
+                    ],
+                },
+                dark: {
+                    ...require("daisyui/src/colors/themes")[
+                        "[data-theme=night]"
+                    ],
+                },
+            },
+        ],
+    },
 };
