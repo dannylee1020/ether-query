dafdce88ccae5ea55e64b9664e2ad26a33092142
Daniel Lee <daniel.lee@circleci.com>
2022-11-01 21:02:13 -0700

    implement dark mode toggle

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,24 @@
 import { GoSearch } from "react-icons/go";
 import { getBlock, getLatestBlock } from "../api/extract";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import Block from "./Block";
 
 const Search = () => {
+    // set state
     const [blockNum, setBlockNum] = useState();
     const [block, setBlock] = useState();
+    const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
+    //dark theme
+    useEffect(() => {
+        useDark();
+    }, [theme]);
+
+    // subscribe data fetching with react query
     const nBlock = useQuery(["block", blockNum], () => {
         getBlock(blockNum);
     });
-
     const latestBlock = useQuery("latest_block", getLatestBlock, {});
 
     const handleClick = async (e) => {
@@ -28,26 +35,47 @@ const Search = () => {
         setBlock(res);
     };
 
+    // dark mode
+    function useDark() {
+        if (
+            theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+            document.documentElement.setAttribute("data-theme", "dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+            localStorage.setItem("theme", "light");
+        }
+
+        console.log(localStorage.getItem("theme"));
+    }
+
+    function toggleDark() {
+        theme === "dark" ? setTheme("light") : setTheme("dark");
+    }
+
+    // handle user input
     function handleInput(e) {
         setBlockNum(e.target.value);
     }
 
     return (
-        <div className="flex flex-col w-100% items-center gap-10">
+        <div className="flex flex-col w-full items-center gap-10 h-screen">
             <div className="flex justify-center items-center mt-10 w-2/3">
                 <input
                     type="text"
                     placeholder="Search by block number / hash"
-                    className="border-2 w-2/3 placeholder:text-sm"
+                    className="border-2 border-slate-300 w-2/3 placeholder:text-sm"
                     onChange={handleInput}
                 ></input>
-                <div className="border-2 h-7 w-8 flex items-center justify-center">
+                <div className="border-2 border-slate-300 h-7 w-8 flex items-center justify-center">
                     <GoSearch onClick={handleClick}></GoSearch>
                 </div>
+                <button onClick={toggleDark}>Toggle</button>
             </div>
-            <div className="w-2/3">
-                {block ? <Block block={block} /> : null}
-            </div>
+            <div>{block ? <Block block={block} /> : null}</div>
         </div>
     );
 };
