9886a6e755365c8386b7d703a1c08a89e5539ed7
Daniel Lee <daniel.lee@circleci.com>
2022-11-13 15:06:16 -0800

    remove queries from this component"

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,41 +1,16 @@
 import { GoSearch } from "react-icons/go";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { BsSun } from "react-icons/bs";
-import { getBlock, getLatestBlock } from "../api/extract";
 import { useState, useEffect } from "react";
-import { useQuery } from "react-query";
 import Block from "./Block";
 
-const Search = () => {
-    // set state
-    const [blockNum, setBlockNum] = useState();
-    const [block, setBlock] = useState();
+const Search = (props) => {
     const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
     //dark theme
     useEffect(() => {
         useDark();
     }, [theme]);
-
-    // subscribe data fetching with react query
-    const nBlock = useQuery(["block", blockNum], () => {
-        getBlock(blockNum);
-    });
-    const latestBlock = useQuery("latest_block", getLatestBlock, {});
-
-    const handleClick = async (e) => {
-        let res;
-
-        if (!blockNum) {
-            await latestBlock.refetch();
-            res = latestBlock.data;
-        } else {
-            await nBlock.refetch();
-            res = nBlock.data;
-        }
-
-        setBlock(res);
-    };
 
     // dark mode
     function useDark() {
@@ -50,17 +25,10 @@ const Search = () => {
             document.documentElement.setAttribute("data-theme", "light");
             localStorage.setItem("theme", "light");
         }
-
-        console.log(localStorage.getItem("theme"));
     }
 
     function toggleDark() {
         theme === "dark" ? setTheme("light") : setTheme("dark");
-    }
-
-    // handle user input
-    function handleInput(e) {
-        setBlockNum(e.target.value);
     }
 
     return (
@@ -70,17 +38,17 @@ const Search = () => {
                     type="text"
                     placeholder="Search by block number / hash"
                     className="border-[1.3px] border-slate-300 w-2/3 placeholder:text-sm border-r-0 h-8 pl-3"
-                    onChange={handleInput}
+                    onChange={props.handleInput}
                 ></input>
-                <button className="btn btn-sm btn-square rounded-none">
-                    <GoSearch onClick={handleClick}></GoSearch>
+                <button className="btn btn-sm btn-square btn-outline rounded-none">
+                    <GoSearch onClick={props.handleClick}></GoSearch>
                 </button>
                 <button onClick={toggleDark} className="text-2xl ml-10">
                     {theme === "dark" ? <BsSun /> : <MdOutlineDarkMode />}
                 </button>
             </div>
 
-            <div>{block ? <Block block={block} /> : null}</div>
+            <div>{props.block ? <Block block={props.block} /> : null}</div>
         </div>
     );
 };
